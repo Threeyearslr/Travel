5798d40f0ce3e23df6884c0d163f30570a6ea6ad
lorry <2416918057@qq.com>
2018-05-04 16:09:28 +0800

    modified:   .eslintrc.js 	modified:   index.html 	modified:   package.json 	modified:   src/App.vue 	deleted:    src/components/HelloWorld.vue 	modified:   src/main.js 	modified:   src/router/index.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import   './assets/styles/reset200802.css';
+import './assets/styles/border.css';
+import fastclick from 'fastclick';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+fastclick.attach(document.body);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // router:router,
   router,
+  //   第一个App是名字，第二个是类型,如果是一样的就省略一个
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+});
+// 路由就是根据网址的不同返回不同的内容给用户
