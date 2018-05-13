37eacb143ae17614f365c487c766b54f04fe7f25
lorry <2416918057@qq.com>
2018-05-13 16:05:28 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+};
+</script>
+
+<style lang="scss" scoped>
+  @import '~styles/varibles';
+  .list{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      position: absolute;
+      right: 0;
+      top: 1.58rem;
+      bottom: 0;
+      width: 0.4rem;
+      .item{
+          text-align: center;
+          line-height: 0.4rem;
+          color: $bgColor;
+      }
+  }
+</style>
